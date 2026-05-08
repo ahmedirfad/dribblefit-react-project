83621137d56a41b273eec5bfef9c21fe6347666d
ahmedirfad <ahmedirfad8@gmail.com>
2026-05-08 09:35:37 +0530

    Redis implemented

--- backend/config/redis.js
+++ backend/config/redis.js
@@ -1,7 +1,11 @@
 const redis = require('redis');
 
 const redisClient = redis.createClient({
-  url: process.env.REDIS_URL
+  url: process.env.REDIS_URL,
+  socket: {
+    tls: true,
+    rejectUnauthorized: false
+  }
 });
 
 redisClient.on('error', (err) => console.error('Redis Error:', err));
